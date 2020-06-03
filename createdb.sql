b27be1dc7935e81cb9c9d9b5fec8f98507772cda
sangboi2 <phtha12@gmail.com>
2020-06-03 15:57:26 +0200

    Improved

--- createdb.sql
+++ createdb.sql
@@ -4,31 +4,31 @@ USE ShoppingDB;
 
 CREATE TABLE Category (
     category_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name varchar(255)
+    name varchar(50)
 );
 CREATE TABLE Postalcode (
     postalcode_id varchar(20) NOT NULL PRIMARY KEY,
-    city varchar(255)
+    city varchar(50)
 );
 CREATE TABLE User (
     user_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    user_name varchar(255) NULL,
-    admin varchar(255) NULL,
-    guest varchar(255) NULL,
-    user varchar(255) NULL,
-    email varchar(255) NOT NULL,
-    password varchar(255) NOT NULL,
+    user_name varchar(50) NULL,
+    admin varchar(50) NULL,
+    guest varchar(50) NULL,
+    user varchar(50) NULL,
+    email varchar(50) NOT NULL,
+    password varchar(50) NOT NULL,
     telephone varchar(20) NULL,
-    street varchar(255) NULL,
+    street varchar(50) NULL,
     postalcode varchar(20) NOT NULL,
-    country varchar(255) NULL,
+    country varchar(50) NULL,
     FOREIGN KEY (Postalcode) REFERENCES Postalcode (postalcode_id)
 );
 CREATE TABLE Shopping (
     shopping_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    product varchar(255) NULL,
+    product varchar(50) NULL,
     image varchar(255) NULL,
-    price varchar(255) NULL,
+    price int NOT NULL,
     description varchar(255) NULL,
     user_id int NOT NULL,
     category_id int NOT NULL,
@@ -37,12 +37,12 @@ CREATE TABLE Shopping (
 );
 CREATE TABLE Product (
     product_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name varchar(255) NULL,
-    image varchar(255) NULL,
+    name varchar(50) NULL,
+    image varchar(50) NULL,
     description varchar(255) NULL,
     price varchar(255) NULL,
-    qty varchar(255) NOT NULL,
-    stock varchar(255) NOT NULL,
+    qty int NOT NULL,
+    stock varchar(50) NOT NULL,
     shopping_id int NOT NULL,
     category_id int NOT NULL,
     FOREIGN KEY (shopping_id) REFERENCES Shopping (shopping_id),
@@ -51,10 +51,10 @@ CREATE TABLE Product (
 );
 CREATE TABLE `Order` (
     order_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    user varchar(255) NULL,
-    product varchar(255) NULL,
-    qty varchar(255) NULL,
-    price varchar(255) NULL,
+    user varchar(50) NULL,
+    product varchar(50) NULL,
+    qty int NOT NULL,
+    price float NOT NULL,
     date_of_purchase DATE NOT NULL,
     delivery_date DATE NOT NULL,
     user_id int NOT NULL,
